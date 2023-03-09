5bea25b0bf963b6c0196f0a2b4bf6db3ebab78b9
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-09 01:21:26 +0100

    Tic Tac Toe Game

---
+++ script.js
@@ -0,0 +1,107 @@
+let box0 = document.querySelector('.box0');
+let box1 = document.querySelector('.box1');
+let box2 = document.querySelector('.box2');
+let box3 = document.querySelector('.box3');
+let box4 = document.querySelector('.box4');
+let box5 = document.querySelector('.box5');
+let box6 = document.querySelector('.box6');
+let box7 = document.querySelector('.box7');
+let box8 = document.querySelector('.box8');
+
+let arg =['x','o'];
+
+// alert('ok');
+
+
+
+
+// alert('ok');
+for(let i in arg){
+    let value = arg[i];
+    box0.addEventListener('click', function getValue(){
+        box0.innerHTML= value;
+        
+    });
+
+    i++;
+
+    box1.addEventListener('click', function getValue(){
+        box1.innerHTML = value;
+        
+    });
+
+    i++;
+    
+    }
+
+
+// box2.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box2.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
+// box3.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box3.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
+// box4.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box4.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
+// box5.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box5.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
+// box6.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box6.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
+// box7.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box7.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
+// box8.addEventListener('click', function getValue(){
+//     // alert('ok');
+//     for(let i=0; i<arg.length; i++){
+//         let value = arg[i];
+//         box8.innerHTML = value;
+//         // alert(value)
+//     }
+
+// });
+
---
+++ script1.js
@@ -0,0 +1,17 @@
+let clicks = document.querySelector('.clicks');
+let result = document.querySelector('.result');
+// let arg =['x','o','y'];
+
+clicks.addEventListener('click', function clickMe(){
+    alert('ok')
+})
+
+
+// for(let i in arg){
+        // click.addEventListener('click', function clickMe(){
+        // let value = arg[i];
+        // alert('ok')    
+        // result.innerHTML=value;
+        // i++;
+    // })
+// }
