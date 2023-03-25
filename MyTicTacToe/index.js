a2bb9a732930b697c8e807449a82a0f3a59cb475
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-25 02:08:45 +0100

    Tic Tac Toe Game

--- MyTicTacToe/index.js
+++ MyTicTacToe/index.js
@@ -1,4 +1,27 @@
-let player_x = "x";
-let player_O = "circle";
+const block = document.getElementsByClassName("box")
+let player_X = true;
+let player_O = false;
+const winCondition = [
+    ['0,1,2'],
+    ['3,4,5'],
+    ['6,7,8'],
+    ['0,3,6'],
+    ['1,4,7'],
+    ['2,5,8'],
+    ['0,4,8'],
+    ['2,4,6'],
+]
+const newBox = Array.from(block);
 
-console.log(player_x)
+newBox.forEach(newBox => newBox.addEventListener("click", function start(){
+        alert(x)
+        if (newBox.textContent === "" || newBox.textContent === "O"){
+            newBox.textContent = "X";
+        } else {
+            newBox.textContent = "O"
+        }
+
+    }
+    
+    // alert(element)
+))
