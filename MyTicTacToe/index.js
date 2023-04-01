0d81502db0fd00417a5ece1061bc8f6ba2dd45f7
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-01 12:30:52 +0100

    Tic Tac Toe done

--- MyTicTacToe/index.js
+++ MyTicTacToe/index.js
@@ -30,7 +30,7 @@ function cellClicked(){
     }
 
    updateCell(this, cellIndex);
-   changePlayer();
+//    changePlayer();
    checkWinner();
 }
 function updateCell(cell, index){
@@ -72,67 +72,12 @@ else{
 }
 }
 function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
     
 }
 
 
-// startGame();
-
-// function startGame(){
-//     cell.forEach(cell => cell.addEventListener("click", cellClicked));
-//     restartBtn.addEventListener("click", restartGame);
-//     statusText.textContent = `${currentPlayer}'s turn`;
-//     running = true;  
-// }
-// function cellClicked() {
-//     const cellIndex = this.getAttribute("cellIndex");
-
-//     if(options[cellIndex] != "" || !running){
-//         return;
-//     }
-
-//     updateCell(this, cellIndex);
-//     checkWinner();
-// }
-// function updateCell(cell, index){
-//     options[index] = currentPlayer;
-//     cell.textContent = currentPlayer;
-// }
-// function changePlayer() {
-//     currentPlayer = (currentPlayer == "X") ? "O" : "X";
-//     statusText.textContent = `${currentPlayer}'s Turn`
-// }
-// function checkWinner(){
-//     let roundWon = false;
-
-//     for(let i = 0; i < winConditions.length; i++){
-//          const condition = winConditions[i];
-//          const cellA = options[condition[0]];
-//          const cellB = options[condition[1]];
-//          const cellC = options[condition[2]];
-
-//          if (cellA == "" || cellB == "" || cellC == "") {
-//             continue
-//          }
-//          if (cellA == cellB && cellB == cellC){
-//             roundWon = true;
-//             break;
-//          }
-//     }
-    
-//     if(roundWon){
-//         statusText.textContent = `${currentPlayer} wins!`
-//         running =false;
-//     }
-//     else if(!options.includes("")) {
-//         statusText.textContent = `Draw!`
-//         running = false;
-//     }
-//     else{
-//         changePlayer()
-//     }
-// }
-
-// function restartGame() {
-     
-// }
